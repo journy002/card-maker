d21160437d6181c9f8a0fac02448e83453ee407f
msoh <journy002@naver.com>
2022-01-10 17:38:45 +0900

    add login component

--- src/service/auth_service.js
+++ src/service/auth_service.js
@@ -1,4 +1,6 @@
+// import import firebase from "firebase/app";으로 해주면 authProvider값을 읽어오지 못한다.
 import firebase from "firebase";
+import firebaseApp from "./firebase";
 
 class AuthService {
   login(providerName) {
@@ -6,7 +8,8 @@ class AuthService {
     // const provider = new firebase.auth.GoogleAuthProvider();
     // Object['key']를 이용해서 인자를 받아오는것 같다.
     const authProvider = new firebase.auth[`${providerName}AuthProvider`]();
-    return firebase.auto().signInWithPopup(authProvider);
+    console.log(authProvider, "provider");
+    return firebaseApp.auth().signInWithPopup(authProvider);
   }
 }
 
--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase/app";
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -5,6 +7,9 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 };
 
+console.log(firebaseConfig, "firebaseconfig");
+
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+console.log(firebaseApp, "firebaseApp");
+export default firebaseApp;
